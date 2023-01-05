c35edd58e97f15cd74cc5a80284fe3852fc5d8c7
Kelly and Marc <kellyandmarc@Kellys-MacBook-Pro.local>
2023-01-05 08:26:36 -0700

    state tests

--- states/tests/factories/state_factory.py
+++ states/tests/factories/state_factory.py
@@ -1,10 +1,12 @@
 import factory
+
 from states.models import State
+
 
 class StateFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = State
-    
+
     state = "CO"
-    flood_cost_percentage = .5
-    monthly_tax = .01
+    flood_cost_percentage = 0.5
+    monthly_tax = 0.01
--- users/tests/factories/user_factory.py
+++ users/tests/factories/user_factory.py
@@ -7,9 +7,9 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
-    username = factory.Faker('email')
-    password = make_password('123')
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    username = factory.Faker("email")
+    password = make_password("123")
     is_staff = True
     is_superuser = True
